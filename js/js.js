141e78b8dbd6f7fd4b51e67b85586aa2a6de7620
Conrad Lim <conradp04@yahoo.com>
2017-04-03 13:02:48 -0700

    Update js.js

--- js/js.js
+++ js/js.js
@@ -1,14 +1,22 @@
-$(function() {
+// $(function() {
 
-    $("html, body").mousewheel(function(event, delta) {
-        var mult = 1;
-        var $this = $(this);
-        if (event.timeStamp - $this.data('oldtimeStamp') < 1000) {
-            //calculate easing here
-            mult = 10000 / (event.timeStamp - $this.data('oldtimeStamp'));
-        }
-        $this.data('oldtimeStamp', event.timeStamp);
-        this.scrollLeft -= (delta) * mult;
-        event.preventDefault();
-    });
-});
+//     $("html, body").mousewheel(function(event, delta) {
+//         var mult = 1;
+//         var $this = $(this);
+//         if (event.timeStamp - $this.data('oldtimeStamp') < 1000) {
+//             //calculate easing here
+//             mult = 10000 / (event.timeStamp - $this.data('oldtimeStamp'));
+//         }
+//         $this.data('oldtimeStamp', event.timeStamp);
+//         this.scrollLeft -= (delta) * mult;
+//         event.preventDefault();
+//     });
+// });
+
+
+$(function() {
+            $("html, body").mousewheel(function(event, delta) {
+                this.scrollLeft -= (delta * 30);
+                event.preventDefault();
+            });
+        });
